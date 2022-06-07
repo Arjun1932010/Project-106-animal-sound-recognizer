57f5cd3df1c53550c1a9494d624b7c89a503d0c5
Arjun1932010 <83018389+Arjun1932010@users.noreply.github.com>
2022-06-07 18:45:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ function startClassification(){
 function modelReady(){
    classifier.classify(gotResults);
 }
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+
+        random_number_cat = Math.floor(Math.random() * 255) + 1;
+        random_number_cow = Math.floor(Math.random() * 255) + 1;
+        random_number_tiger = Math.floor(Math.random() * 255) + 1;
+        random_number_dog = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("label_result").innerHTML = "I can hear _  " + results[0].label
+        document.getElementById("label_confidence").innerHTML = "Accuracy _" + (results[0].confidence * 100).toFixed(2) + "%"
+
+
+        document.getElementById("label_result").style.color =
+            "rgb(" + random_number_cat + "," + random_number_cow + "," + random_number_tiger +","+random_number_dog+ ")";
+        document.getElementById("label_confidence").style.color =
+            "rgb(" + random_number_cat + "," + random_number_cow + "," + random_number_tiger + "," + random_number_dog + ")";
+    }
+}
